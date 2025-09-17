c2f830893fcc4c125bfe1e4be0192218c0e899a8
Paulo Junior <144707609+Pauloj2@users.noreply.github.com>
2025-09-17 17:41:47 -0300

    Adição do método de listar no controlador e ajuste para listar na interface

--- desenvWebll/src/main/java/com/example/desenvWebll/controller/PlantioController.java
+++ desenvWebll/src/main/java/com/example/desenvWebll/controller/PlantioController.java
@@ -1,14 +1,21 @@
 package com.example.desenvWebll.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.desenvWebll.service.PlantioService;
 
 @Controller
 public class PlantioController {
 
+    @Autowired
+    private PlantioService plantioService;
+
     @GetMapping("/plantio")
-    public String listarPlantios() {
-        return "/plantio/index";
+    public String index(Model model) {
+        model.addAttribute("plantiosList", plantioService.getAllPlantios());
+        return "plantio/index";
     }
-    
 }
